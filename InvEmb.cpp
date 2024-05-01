81c1274299fad9f2eff0722d48d1a0fa4c8eded2
Max Makarov <ufamax2@gmail.com>
2024-05-01 18:39:58 +0500

    Вроде настроил плюсы

--- InvEmb.cpp
+++ InvEmb.cpp
@@ -1,3 +1,26 @@
+#include<vector>
+#include<math.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+using namespace std;
+class TargettingMethod
+{
+public:
+	TargettingMethod();
+	TargettingMethod(double tempQcon, double tempEIx,double tempL, double tempH);
+	double TrueY(double x);
+	double F(double z, double tempL, double v);
+	double FindP();
+	double RuK(double tempL, double tempP);
+protected:
+private:
+	double q_con;
+	double EI_x;
+	double l;
+	double h;
+	double p;
+};
 TargettingMethod::TargettingMethod()
 {
     q_con = 1.;
@@ -65,4 +88,8 @@ double TargettingMethod::RuK(double tempL, double tempP)
     }
 
     return un;
+}
+int main()
+{
+    cout << "FO";
 }
